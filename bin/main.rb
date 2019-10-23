5d23691005e6574dbc24fd9d0ac61bbe1c36d2af
Edwin Moradian <edwin.moradian90@gmail.com>
2019-10-23 12:36:23 -0700

    seond-milestone

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/env ruby
 
 puts 'Hello World!'
+puts 'Welcome to tic-tac-toe in Ruby'
+puts 'Set count = 0'
+puts 'player = count % 2 == 0 ? "X" : "O"'
+puts 'ENTER WHILE LOOP: while count < 9'
+puts 'check count'
+puts 'check player X or O'
+puts 'ask for move'
+puts 'get.chomp move'
+puts 'pass move into array of moves'
+puts 'display on board the move'
+puts 'check after move if board is in a win condition'
+puts 'if board is in win condition, return congrats player youve won, else continue'
+puts 'add one to count: count += 1'
+puts 'if count is = 9 return " Game is a draw please start over"'
+
